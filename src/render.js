c9a763bbe41f8c2e75dd239d321858f046374ad0
dolamore <antonbalkouski@gmail.com>
2025-01-30 10:59:41 +0300

    feat: make vars separate class

--- src/render.js
+++ src/render.js
@@ -1,14 +1,5 @@
 import * as Tone from 'https://cdn.skypack.dev/tone';
-
-const sounds = [
-    null, // No sound
-    new Tone.Synth({oscillator: {type: 'sine'}}).toDestination(),
-    new Tone.Synth({oscillator: {type: 'triangle'}}).toDestination(),
-    new Tone.Synth({oscillator: {type: 'square'}}).toDestination(),
-    new Tone.Synth({oscillator: {type: 'sawtooth'}}).toDestination()
-];
-const noteSizes = ['1n', '2n', '4n', '8n', '16n', '32n', '64n']; // Default to '4n' (quarter note)
-const noteMultipliers = [4, 2, 1, 0.5, 0.25, 0.125, 0.0625];
+import {sounds, noteSizes, noteMultipliers} from './vars.js';
 
 let selectedSounds = [1, 1, 1, 1]; // Default to the first sound for all notes
 let soundSettings = [
