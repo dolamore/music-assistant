4b2dec55b99e8baa95df7ffe497c977e9d555936
dolamore <antonbalkouski@gmail.com>
2025-03-11 10:58:21 +0100

    feat: add webpack and first react component

--- src/components/bpmControls.js
+++ src/components/bpmControls.js
@@ -1,10 +1,11 @@
-import {elements} from "../vars.js";
-
 export default function BpmControls({metronomeManager}) {
     return (
         <div className="bpm-controls">
             <IncreaseBpmButton/>
+            <IncreaseFiveBpmButton/>
             <BpmInput/>
+            <DecreaseFiveBpmButton/>
+            <DecreaseBpmButton/>
         </div>
     )
 }
@@ -20,6 +21,24 @@ function BpmInput({metronomeManger}) {
         <input type="number" id="bpm-input" value={metronomeManger.bpm}
                onChange={metronomeManger.handleNewBPM}
                onBlur={metronomeManger.elementsManager.handleBpmInputChanges}/>
+    )
+}
+
+function DecreaseBpmButton(metronomeManager) {
+    return (
+        <button onClick={() => metronomeManager.handleBpmChange(-1)}>-1</button>
+    )
+}
+
+function IncreaseFiveBpmButton(metronomeManager) {
+    return (
+        <button onClick={() => metronomeManager.handleBpmChange(5)}>+5</button>
+    )
+}
+
+function DecreaseFiveBpmButton(metronomeManager) {
+    return (
+        <button onClick={() => metronomeManager.handleBpmChange(-5)}>-5</button>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Metronome from "./Metronome.js";
+import BpmControls from "./components/bpmControls.js";
 
 // Находим div с id="root" и рендерим в него React-приложение
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Metronome />);
+root.render(
+    <Metronome>
+        <BpmControls/>
+    </Metronome>
+);
