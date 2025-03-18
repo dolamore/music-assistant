ff511f0194fb3e9d38dbff54ef13890e90fc8ce3
dolamore <antonbalkouski@gmail.com>
2025-03-18 15:55:00 +0100

    feat: add hidden class to pendulum

--- src/components/pendulum.js
+++ src/components/pendulum.js
@@ -4,7 +4,10 @@ import {inject} from "mobx-react";
 
 export default inject("metronomeManager")(observer(function Pendulum({metronomeManager}) {
     return (
-        <div className="pendulum-container container">
+        <div
+            className={`pendulum-container
+                        container
+                        ${!metronomeManager.visualEffectsManager.isPendulumVisible ? 'hidden' : ''}`}>
             <div className="horizontal-bar">
                 <div className="pendulum"></div>
             </div>
