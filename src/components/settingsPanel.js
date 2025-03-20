d218d8555e641f659268093b80d91fa9366e81e9
dolamore <antonbalkouski@gmail.com>
2025-03-20 11:46:13 +0100

    feat: add settings panel to react

--- src/components/settingsPanel.js
+++ src/components/settingsPanel.js
@@ -6,21 +6,24 @@ import {defaultSoundSettings} from "../vars.js";
 export default inject("metronomeManager")(observer(function SettingsPanel({metronomeManager}) {
     const indices = Array.from({length: metronomeManager.numberOfBeats},
         (_, i) => i + 1);
-
+    const numColumns = Object.keys(defaultSoundSettings).length + 1; // +1 for oscillator
     return (
         <div id="settings-panel"
              className={`${metronomeManager.elementsManager.isSettingsPanelVisible ? '' : 'hidden'}
                         container`}>
             <h2>Sound Settings</h2>
-            <div className="sound-settings">
+            <div className="sound-settings-grid" style={{ gridTemplateColumns: `150px repeat(${numColumns}, 1fr)` }}>
                 <div className="labels">
                     <span>Beat</span>
                     <span>Oscillator</span>
+                    {Object.keys(defaultSoundSettings).map(key => (
+                        <span key={key}>{key.charAt(0).toUpperCase() + key.slice(1)}</span>
+                    ))}
                 </div>
+                {indices.map(index => (
+                    <SoundRow key={`sound-row-${index}`} metronomeManager={metronomeManager} index={index}/>
+                ))}
             </div>
-            {indices.map(index => (
-                <SoundRow key={`sound-row-${index}`} metronomeManager={metronomeManager} index={index}/>
-            ))}
             <button id="save-settings-button"
                     onClick={() => metronomeManager.elementsManager.toggleSettingsPanel()}
             >Save
