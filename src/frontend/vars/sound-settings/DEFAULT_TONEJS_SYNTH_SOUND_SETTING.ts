685b158522ac557bc03e9fe1c1bfe5e3c27068a7
dolamore <antonbalkouski@gmail.com>
2025-04-30 13:46:25 +0400

    feat: add min and max values for synth settings

--- src/frontend/vars/sound-settings/DEFAULT_TONEJS_SYNTH_SOUND_SETTING.ts
+++ src/frontend/vars/sound-settings/DEFAULT_TONEJS_SYNTH_SOUND_SETTING.ts
@@ -1,11 +1,10 @@
 export const DEFAULT_TONEJS_SYNTH_SOUND_SETTINGS = [
-    {key: 'frequency', value: 440, label: "Frequency", minValue: 0, maxValue: 1000},
-    {key: 'detune', value: 0, label: "Detune"},
-    {key: 'phase', value: 0, label: "Phase"},
-    {key: 'volume', value: 0, label: "Volume"},
-    {key: 'attack', value: 0.001, label: "Attack"},
-    {key: 'decay', value: 0.1, label: "Decay"},
-    {key: 'sustain', value: 0, label: "Sustain"},
-    {key: 'release', value: 0.1, label: "Release"},
-];
-//TODO: проверить значения min, max
+    { key: 'frequency', value: 440, label: "Frequency", minValue: 0, maxValue: 20000 }, // частота от 0 до 20кГц
+    { key: 'detune', value: 0, label: "Detune", minValue: -1200, maxValue: 1200 }, // ±1 октава (в центах)
+    { key: 'phase', value: 0, label: "Phase", minValue: 0, maxValue: 360 }, // градусы
+    { key: 'volume', value: 0, label: "Volume", minValue: -60, maxValue: 0 }, // dB, обычно от -∞ до 0
+    { key: 'attack', value: 0.001, label: "Attack", minValue: 0.001, maxValue: 10 }, // секунды
+    { key: 'decay', value: 0.1, label: "Decay", minValue: 0.001, maxValue: 10 }, // секунды
+    { key: 'sustain', value: 0, label: "Sustain", minValue: 0, maxValue: 1 }, // пропорция
+    { key: 'release', value: 0.1, label: "Release", minValue: 0.001, maxValue: 10 }, // секунды
+]
