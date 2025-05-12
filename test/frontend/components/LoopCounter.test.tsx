ce8ea776aae537886413c99b6db704279b2df418
dolamore <antonbalkouski@gmail.com>
2025-05-12 16:06:52 +0200

    feat: test loop counter

--- test/frontend/components/LoopCounter.test.tsx
+++ test/frontend/components/LoopCounter.test.tsx
@@ -1,23 +1,46 @@
 import * as React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react'; // импортируем act отсюда
 import LoopCounter from '../../../src/frontend/components/LoopCounter';
-import {describe, expect, it} from "@jest/globals";
-import {MetronomeManager} from "../../../src/frontend/managers/MetronomeManager";
-
-const mockMetronomeManager = {
-    audioEngine: {
-        loopCount: 5
-    }
-};
+import { describe, expect, it } from "@jest/globals";
+import { MetronomeManager } from "../../../src/frontend/managers/MetronomeManager";
+import { observable } from 'mobx';
 
 describe('LoopCounter', () => {
-    it('should show the right amount of played cycles', () => {
+    it('should show the right amount of played cycles and update when loopCount changes', () => {
+        const mockMetronomeManager = {
+            audioEngine: observable({
+                loopCount: 5
+            })
+        };
+
         render(<LoopCounter metronomeManager={mockMetronomeManager as MetronomeManager} />);
 
-        // Проверяем, что счетчик отображает правильное значение
         expect(screen.getByText('5')).toBeInTheDocument();
 
-        // Проверяем заголовок
-        expect(screen.getByText('Loops played:')).toBeInTheDocument();
+        act(() => {
+            mockMetronomeManager.audioEngine.loopCount = 8;
+        });
+
+        expect(screen.getByText('8')).toBeInTheDocument();
+    });
+
+    it('should update when store changes', () => {
+        const metronomeManager = observable({
+                audioEngine: {
+                    loopCount: 5
+                }
+        });
+
+        render(
+                <LoopCounter metronomeManager={metronomeManager as MetronomeManager}/>
+        );
+
+        expect(screen.getByText('5')).toBeInTheDocument();
+
+        act(() => {
+            metronomeManager.audioEngine.loopCount = 12;
+        });
+
+        expect(screen.getByText('12')).toBeInTheDocument();
     });
 });
